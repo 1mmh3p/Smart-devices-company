66f0db3aa111e2cfa8a6fc6adeed237f24198f8c
Mustafa Elhuseyyen <136298486+1mmh3p@users.noreply.github.com>
2024-06-23 14:35:06 +0300

    Add files via upload

---
+++ inc/conn.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+  <title> الموقع الشخصي </title>
+  <meta charset="utf-8"/>
+  </head>
+  <body>
+<?php
+$connection=mysqli_connect("localhost","root","Vctvxesc101112#","mstore");
+if(!$connection)
+	echo"فشل الاتصال بقاعدة البيانات";
+?>
+</body>
+</html>
